2680bd1a766f1cf22348206fe407d3c01afae72f
berkcodes <berk@tekbyte.net>
2020-04-28 09:15:06 +1000

    added a few functions

--- C_Proj_Main.c
+++ C_Proj_Main.c
@@ -1,17 +1,39 @@
-/* my test C Program */
+//
+// Lab 3 - EEET2096
+//
+// Authors: 
+//   Berk Dogan, s3717496
+//   Engin Akil, 
+//
+// Last Modified: 28 Aug 2020
 
 #include "stm32f439xx.h"
 
 
-int Sum( int a, int b)  // a simple function for testing
-{ int c;
+int Sum( int a, int b) { 
+	int c;
 	c=a+b;
 	return c;
 }
 
+/**
+	Delay function, pass in a number.
+*/
+void myDelay(long delay) {
+	for (long i = 0; i <delay; i++) {
+	}
+}
 
-int main ( void)
-{	
+/**
+	System Initialisation Function
+*/
+void SystemInit(void) {
+	RCC->AHB1ENR |= 1<<7; // enable the AHB clock for port h
+	
+}
+
+
+int main (void) {	
 	
 	int x,y,z, s;
 	
@@ -28,7 +50,8 @@ int main ( void)
 	RCC->AHB1ENR |= 0x000001F8;
 	GPIOD->ODR = z;
 	
-
+	
+	// infinite loop
 	for(; ;)
 	{;} /* dead loop*/
 }
