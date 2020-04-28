a3f0c2fa7c735b1aae198ef951f579b8814dffdb
berkcodes <berk@tekbyte.net>
2020-04-28 16:17:55 +1000

    some more changes

--- C_Proj_Main.c
+++ C_Proj_Main.c
@@ -30,84 +30,71 @@ int main (void) {
 		
 	int counter = 0;
 	int fakeIDR = 0;
+	int buttonRead;
 	
 	
 	// enable the clock	(portb and portf)
 	RCC->AHB1ENR |= (1<<1) | (1<<6);
 	
-	// Setup PORTB
-	GPIOB->MODER &= ~(3UL<<4);		//  clear mode bits
-	GPIOB->MODER |= 1UL<<4;				// set mode to output
-	GPIOB->OTYPER &= ~(1UL<<2);		// select push-pull output
+	// Setup the mode register for output (portb0)
+	GPIOB->MODER |= GPIO_MODER_MODER0_0;
+	GPIOB->MODER &= ~(GPIO_MODER_MODER0_1);
+	// Setup the mode register for output (portb1)
+	GPIOB->MODER |= GPIO_MODER_MODER1_0;
+	GPIOB->MODER &= ~(GPIO_MODER_MODER1_1);
 	
-	// Setup PORTF
-	RCC->AHB2ENR |= RCC_AHB1ENR_GPIOFEN;
-	GPIOF->MODER &= ~(3UL<<4);		// clear the mode bits
-	GPIOF->MODER |= 1UL<<4;				// set the mode to output
-	GPIOF->OTYPER &= ~(1UL<<2);		// select push-pull output
+	// Output type register
+	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT0);
+	GPIOB->OTYPER &= ~(GPIO_OTYPER_OT1);
+	
+	// Speed
+	//GPIOB->OSPEEDR |= GPIO_OSPEEDR_OSPEEDR0_0 | GPIO_OSPEEDR_OSPEEDR0_1;
+	
 
 	// Main Code
 	while (1) {
 		
-		switch (counter) {
-			
-			/***************************
-			Case 0: Button A and B is pressed
-				0b00 = 0
-				Both LEDs are off
-			*/
-			case 0:
-				GPIOB->ODR &=(0x00000000); // turn off portb
-				GPIOF->ODR &=(0x00000000); // turn off portf
-			
-				fakeIDR = (GPIOB->IDR) & (GPIOF->IDR);
-
+		if ((counter % 10) == 0)
+			fakeIDR++;
+		
+		buttonRead = GPIOB->IDR; 		// real read of button A & B
+		buttonRead = fakeIDR & 0x3;	// mask the rest of the bits, keep the last two bits
+		
+		switch (buttonRead) {
+						
+			case 0x0:
+				// buttons 0,0
+				// led 6 off, led 7 off
+				GPIOB->ODR |= (1<<8);
+				GPIOF->ODR |= (1<<8);
 				break;
 			
-			/************************
-			Case 1: Button A is pressed
-				0b01 = 1
-				LED6 = OFF, LED7 = ON
-			*/
-			case 1:
-				// led7 on
-				GPIOF->ODR |= (1<<8);				// output to turn on LED7 (PF8)
-				GPIOB->ODR &= (0x00000000);		// set it all zero
-				
-				fakeIDR = (GPIOB->IDR) & (GPIOF->IDR);
 			
+			case 0x1:
+				// buttons 0,1
+				// led 6 off, led 7 on
+				GPIOB->ODR |= (1<<8);
+				GPIOF->ODR &= ~(1<<8);
 				break;
 			
-			/*************************
-			CASE 2: Button B is pressed
-				0b10 = 2
-				LED6 = ON, LED7 = OFF
-			*/
-			case 2:
-				// led 6 on
-				GPIOF->ODR |= (0x00000000);  // turn off pf8
-				GPIOB->ODR &= (1<<8);		// turn on pb8
-			
-				fakeIDR = (GPIOB->IDR) & (GPIOF->IDR);
+			case 0x2:
+				// buttons 1,0
+				// led 6 on, led 7 off
+				GPIOB->ODR &= ~(1<<8);
+				GPIOF->ODR |= (1<<8);
 				break;
 			
-			/*
-			CASE 3: No button is pressed
-				0b11 = 3
-				LED 6 = OFF, LED 7 = on/off blinking (1s)
-			*/
+			
 			case 3:
-				// led 6 off, led 7 blinking
-				GPIOF->ODR |= (1<<8); // turn on PF8
-				GPIOB->ODR &= (0x00000000);		// set it all zero
-				myDelay(5); // temporary delay
-				GPIOF->ODR &= (0x00000000); // turn off PF8
+				// buttons 1,1
+				// led 6 off, led 7 on (flashing)
+				GPIOB->ODR |= (1<<8);
+				GPIOF->ODR &= ~(1<<8);
+				myDelay(10); // wait 10 units
+				GPIOF->ODR |= (1<<8);
 
-				fakeIDR = (GPIOB->IDR) & (GPIOF->IDR);
-				break;
+
 		}
-		counter++; // increment counter to next state
-		if (counter == 4) { counter = 0; } // check if it overruns
-		fakeIDR=0; // reset fakeIDR value
+		counter++; // increment counter
 	}
 }
