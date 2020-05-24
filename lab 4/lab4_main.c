7a9a68641b1e5ec1a325eb3206e8ccac23d6bdc7
berkcodes <!Kr3b@99github.c0m##>
2020-05-24 14:19:34 +1000

    Added UART Tx and Rx

--- lab 4/lab4_main.c
+++ lab 4/lab4_main.c
@@ -12,27 +12,97 @@
 #include <stdbool.h>
 
 
-// Global variables are defined here...
+/****************************
+General variables
+****************************/
 int current, humidity;
 int light;
+bool lightStatus; // might use these to show that the fan is working or not
+bool fanStatus;		// same for fan
+/***********************************
+UART SETUP
+	- Refer to tutorial 8, slide 9 for UART receiving interrupt. 
+	- Slide 7 for Tx.
+*/
+char rx_array[2];				// AAAAA
+int rx_index;						// USED FOR RECEIVING
+int nextByteIsCmdFlag;	// detecting if a command is coming from the UART
+bool lightCommandFlag; 	// set if a light command is received
+bool lightCommand;			// light command 1 or 0
+bool fanCommandFlag;		// set if fan command is received
+bool fanCommand;				// fan command 1 or 0
 
-bool lightCommandFlag;
-bool lightToggleFlag;
-
-int lightCommand;
-int lightSwitchStatus;
+char tx_array[3];				// for transmission
+int tx_index = 0;				
+bool tx_finished = false;
 
 // ADC interrupt routine
 
 // SysTick interrupt routines
 
-// UART interrupt routines
 
 
+/****************************
+USART IRQ Receive (Rx)
+****************************/
+void USART6_IRQHandler_Rx() {
+	char header=0xAD;
+	char rx_char;
+	
+	if (USART6->SR & ((1<<5)==1)) {
+		// if the RXD register is full
+		rx_char = USART6->DR; // rx_char has received the byte
+		
+		if (rx_char == header) {
+			rx_index = 0;
+			rx_array[rx_index] = rx_char;
+			rx_index++; // move to next position
+			nextByteIsCmdFlag = 1;
+		}
+		else {
+			if (nextByteIsCmdFlag) {
+				// the following byte is part of a command
+				
+				rx_array[rx_index] = rx_char;
+				rx_index++;
+				
+				if (rx_index == 2) {
+					// since the full command is received, setup the command flags and commands...
+					// ...
+					
+					rx_index = 0;
+					nextByteIsCmdFlag = 0;
+				}
+			}
+			
+		}
+		
+		
+	}
+	
+}
 
-/**
-	Delay function, pass in a number.
-*/
+
+/****************************
+USART IRQ Transmission (Tx)
+****************************/
+void USART6_IRQHandler_Tx() { 
+	
+	if (USART6->SR & ((1<<7)==0)) {
+		// if TXD register is empty
+		USART6->DR = tx_array[tx_index]; // transmit 1 byte!
+		tx_index++;	// move ot next byte!
+		
+		if(tx_index == 3) {
+			tx_finished = true;
+			tx_index = 0;
+		}
+	}
+	
+}
+/****************************
+USART IRQ Transmission (Tx)
+****************************/
 void myDelay(long delay) {
 	for (long i = 0; i <delay; i++) {
 	}
